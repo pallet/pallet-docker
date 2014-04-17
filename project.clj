0b06bf9b331bd1c97938d8cd5be2ca308a5a6d6b
Hugo Duncan <hugo@hugoduncan.org>
2014-04-17 11:22:48 -0400

    Depend on latest docker-crate snapshot

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.palletops/pallet "0.8.0-RC.1"]
-                 [com.palletops/docker-crate "0.8.0-alpha.1"]])
+                 [com.palletops/docker-crate "0.8.0-SNAPSHOT"]])
